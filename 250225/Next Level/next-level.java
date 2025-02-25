3e48f453b256e86ca61081e3734274b992b1904b
CHAN <teochan@naver.com>
2025-02-25 09:59:24 +0900

    Title: Next Level, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250225/Next Level/next-level.java
+++ 250225/Next Level/next-level.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 class User{
     String id;
     int level;
-    
-    public User(){
-        this.id="codetree";
-        this.level=10;
+    public User(String id, int level){
+        this.id=id;
+        this.level=level;
     }
 }
 public class Main {
@@ -13,13 +12,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String id = sc.next();
         int level = sc.nextInt();
-
-        User user= new User();
-        System.out.println("user "+user.id+" lv "+user.level);
-        user.id="hello";
-        user.level=28;
-          System.out.println("user "+user.id+" lv "+user.level);
-
-          
+        User user1= new User("codetree",10);
+       System.out.println("user "+user1.id+" lv "+user1.level);
+       User user2= new User(id, level);
+        System.out.println("user "+user2.id+" lv "+user2.level);
     }
 }
