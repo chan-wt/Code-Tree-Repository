e514d0f89018232963ab828e34b9d46dc7c331ca
CHAN <teochan@naver.com>
2025-03-06 14:40:30 +0900

    Title: Date to Date, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250306/Date to Date/date-to-date.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int CountDay=0;
+       int [] Month_Day=new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+       while(true){
+        if(m1==m2 && d1==d2){
+            break;
+        }
+       CountDay++;
+       d1++;
+       if(d1>Month_Day[m1]){
+        m1++;
+        d1=0;
+       }
+       }
+       System.out.print(CountDay);
+    }
+}
