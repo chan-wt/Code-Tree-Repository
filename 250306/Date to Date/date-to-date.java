31dab104ad74c78a4f256964a27c1012012a4f08
CHAN <teochan@naver.com>
2025-03-06 16:32:14 +0900

    Title: Date to Date, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 250306/Date to Date/date-to-date.java
+++ 250306/Date to Date/date-to-date.java
@@ -6,23 +6,18 @@ public class Main {
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        int CountDay=0;
+        int CountDay=1;
        int [] Month_Day=new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
-       if(m1==m2 && d1==d2){
-        CountDay=1;
-       }
-       else{
        while(true){
         if(m1==m2 && d1==d2){
             break;
         }
-       CountDay++;
-       d1++;
-       if(d1>Month_Day[m1]){
-        m1++;
-        d1=0;
-       }
-       }
+        CountDay++;
+        d1++;
+        if(d1>Month_Day[m1]){
+            m1++;
+            d1=1;
+        }
        }
        System.out.print(CountDay);
     }
