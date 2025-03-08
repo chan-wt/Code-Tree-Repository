aa2763e2b26fa141b52c902fc88b3320967bc4cf
CHAN <teochan@naver.com>
2025-03-08 16:21:57 +0900

    Title: 요일 맞추기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250308/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+
+     public static int [] Month_Day= new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+    public static int Function(int m, int d){
+        int totalDay=0;
+        for(int i=1; i<m; i++){
+             totalDay+= Month_Day[i];
+        }
+        totalDay+=d;
+        return totalDay;
+    }
+    public static String WhichDay(int m2, int d2, int m1, int d1){
+        if((Function(m2,d2)- Function(m1,d1))%7==0)
+       return "Mon";
+       else if((Function(m2,d2)- Function(m1,d1))%7==1)
+       return "Tue";
+       else if((Function(m2,d2)- Function(m1,d1))%7==2)
+       return "Wed";
+       else if((Function(m2,d2)- Function(m1,d1))%7==3)
+       return "Thu";
+       else if((Function(m2,d2)- Function(m1,d1))%7==4)
+       return "Fri";
+       else if((Function(m2,d2)- Function(m1,d1))%7==5)
+       return "Sat";
+       else 
+       return "Sun";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+       System.out.print(WhichDay(m2,d2,m1,d1));
+    }
+}
