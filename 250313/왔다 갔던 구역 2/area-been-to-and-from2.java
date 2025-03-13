7868017f22c354490928436ece32a238683d0aca
CHAN <teochan@naver.com>
2025-03-13 14:33:03 +0900

    Title: 왔다 갔던 구역 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 250313/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250313/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,46 +1,59 @@
 import java.util.Scanner;
-public class Main {
-    public static final int MAX_N=2500;
-    public static int [] arr= new int[MAX_N+1];
-    public static int [] x= new int[101];
-    public static String [] direction= new String[101];
+
+public class Main {    
+    public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
+    public static final int MAX_N = 100;
+    
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+    
+    public static int[] checked = new int[MAX_R + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N=sc.nextInt();
-
-      for(int i=0; i<N; i++){
-        x[i]=sc.nextInt();
-        direction[i]=sc.next();
-      }
-       int [] A= new int[10000];
-       for(int i=0; i<N; i++){
-        A[0]=1000;
-        if(direction[i].charAt(0)=='R'){
-          A[i+1]=A[i]+x[i];
-        }
-        else{
-          A[i+1]=A[i]+(x[i]*(-1));
+        // 입력
+        n = sc.nextInt();
+        
+        // 현재 위치
+        int cur = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int distance = sc.nextInt();
+            char direction = sc.next().charAt(0);
+            
+            if(direction == 'L') {
+                // 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+                x1[i] = cur - distance;
+                x2[i] = cur;
+                cur -= distance;
+            }
+            else {
+                // 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+                x1[i] = cur;
+                x2[i] = cur + distance;
+                cur += distance;
+            }
+            
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
         }
         
-      }
-     for(int i=0; i<N; i++){
-        if(direction[i].charAt(0)=='R'){
-        for(int j=A[i]; j<A[i+1]; j++){
-            arr[j]=arr[j]+1;
-        }
-        }
-        else{
-            for(int j=A[i]; j>A[i+1]; j--){
-                arr[j]=arr[j]+1;
-            }
-        }
-     }
-     int cnt=0;
-     for(int i=0; i<=2001; i++){
-        if(arr[i]>=2){
-            cnt++;
-        }
-     }
-     System.out.print(cnt);
-}
+        // 구간을 칠해줍니다.
+        // 구간 단위로 진행하는 문제이므로
+        // x2[i]에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = x1[i]; j < x2[i]; j++)
+                checked[j]++;
+        
+        // 2번 이상 지나간 영역의 크기를 구합니다.
+        int cnt = 0;
+        for(int i = 0; i <= MAX_R; i++)
+            if(checked[i] >= 2)
+                cnt++;
+        
+        System.out.print(cnt);
+    }
 }
