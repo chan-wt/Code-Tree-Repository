a9ee1b60057968272bb50c1d4dd37b6d078a95ed
CHAN <teochan@naver.com>
2025-03-13 14:17:22 +0900

    Title: 왔다 갔던 구역 2, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250313/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250313/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,30 +1,42 @@
 import java.util.Scanner;
 public class Main {
-    public static final int MAX_N=2000;
+    public static final int MAX_N=2500;
     public static int [] arr= new int[MAX_N+1];
     public static int [] x= new int[101];
     public static String [] direction= new String[101];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N=sc.nextInt();
-      for(int i=1; i<=N; i++){
+
+      for(int i=0; i<N; i++){
         x[i]=sc.nextInt();
         direction[i]=sc.next();
       }
+       int [] A= new int[10000];
+       for(int i=0; i<N; i++){
+        A[0]=1000;
+        if(direction[i].charAt(0)=='R'){
+          A[i+1]=A[i]+x[i];
+        }
+        else{
+          A[i+1]=A[i]+(x[i]*(-1));
+        }
+        
+      }
      for(int i=0; i<N; i++){
-        if(direction[i+1].charAt(0)=='R'){
-        for(int j=1000+x[i]; j<=1000+x[i+1]; j++){
+        if(direction[i].charAt(0)=='R'){
+        for(int j=A[i]; j<A[i+1]; j++){
             arr[j]=arr[j]+1;
         }
         }
         else{
-            for(int j=1000+x[i]; j>=1000-x[i+1]; j--){
+            for(int j=A[i]; j>A[i+1]; j--){
                 arr[j]=arr[j]+1;
             }
         }
      }
      int cnt=0;
-     for(int i=0; i<1000; i++){
+     for(int i=0; i<=2001; i++){
         if(arr[i]>=2){
             cnt++;
         }
