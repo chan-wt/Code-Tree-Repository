cdf577d0ba75cbc229f76a7df6323e253e725270
CHAN <teochan@naver.com>
2025-03-19 20:30:21 +0900

    Title: 사각형의 총 넓이 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250319/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250319/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,37 +1,19 @@
 import java.util.Scanner;
-
 public class Main {
+    public static final int MAX_N=100;
+    public static final int D=10;
+    public static int[] x1=new int[MAX_N];
+    public static int[] x1=new int[MAX_N];
+
+    int [][] sqaure=new int[2*MAX_N+1][2*MAX_N+1];
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        int[][] arr = new int[201][201];
-        int offset = 100;
-        int cnt = 0;
-
-        for (int k = 0; k < n; k++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-
-            for (int i = x1; i < x2; i++) { //사각형 채우기
-                for (int j = y1; j < y2; j++) {
-                    arr[i + offset][j + offset] = 1;
-                }
-            }
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=x1[i]; j<x2[i]; j++)
+            for(int k=y1[i]; k<y2[i]; k++)
+            sqaure[j][k]++;
         }
-        
-        for (int l = 0; l < 201; l++) {
-            for (int o = 0; o < 201; o++) {
-                if(arr[l][o]>0){
-                    cnt++;
-                }
-            }
-        }
-
-        System.out.println(cnt);
     }
 }
