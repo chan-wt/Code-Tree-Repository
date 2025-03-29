c101c56b4fa87edda01ab24ee3124e6e160b6c41
CHAN <teochan@naver.com>
2025-03-29 19:27:53 +0900

    Title: 연속되는 수 2, Time: 183ms, Memory: 13MB, Status: Passed - Codetree

--- 250329/연속되는 수 2/continuous-number2.java
+++ 250329/연속되는 수 2/continuous-number2.java
@@ -16,6 +16,9 @@ public class Main {
          for(int i=1; i<n; i++){
             if(arr[i-1]==arr[i]){
             cnt++;
+            if(cnt>max){
+                max=cnt;
+                }
             }
             else if( i==0 ||arr[i]!=arr[i-1])
             {   
